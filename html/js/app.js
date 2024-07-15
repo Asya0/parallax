a24f43aaea11059064f582568f534569283080ad
Asya0 <anya.gerasimenko763@mail.ru>
2024-07-15 22:32:42 +0600

    feat: add project

---
+++ html/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("mousemove", (e) => {
+  document.documentElement.style.setProperty(
+    "--move-x",
+    `${(e.clientX - window.innerWidth / 2) * -0.005}deg`
+  );
+  document.documentElement.style.setProperty(
+    "--move-y",
+    `${(e.clientY - window.innerHeight / 2) * -0.01}deg`
+  );
+});
